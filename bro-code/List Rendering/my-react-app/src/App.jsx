d22efb9518d40f6025b836f9d91d16ace4d8b038
Vikas Pandharinath Shirsath <156660115+vikas47s@users.noreply.github.com>
2025-03-29 19:36:29 +0530

    add

--- bro-code/List Rendering/my-react-app/src/App.jsx
+++ bro-code/List Rendering/my-react-app/src/App.jsx
@@ -2,11 +2,18 @@ import List from "./List"
 
 function App() {
 
+  const fruits = [{id: 1, name: "apple", calories: 95}, 
+    {id:2, name: "orange", calories: 45}, 
+    {id:3, name: "banana", calories: 105}, 
+    {id:4, name: "coconut", calories: 159}, 
+    {id:5, name: "pineapple", calories: 37}];
+
+
   return ( 
     <>
-      <List/>
+      <List items={fruits} category=""/>
     </>
   )
 }
-
+  
 export default App
--- bro-code/List Rendering/my-react-app/src/List.jsx
+++ bro-code/List Rendering/my-react-app/src/List.jsx
@@ -1,11 +1,11 @@
 
 function List() {
 
-    const fruits = [{id: 1, name: "apple", calories: 95}, 
-                    {id:2, name: "orange", calories: 45}, 
-                    {id:3, name: "banana", calories: 105}, 
-                    {id:4, name: "coconut", calories: 159}, 
-                    {id:5, name: "pineapple", calories: 37}];
+    // const fruits = [{id: 1, name: "apple", calories: 95}, 
+    //                 {id:2, name: "orange", calories: 45}, 
+    //                 {id:3, name: "banana", calories: 105}, 
+    //                 {id:4, name: "coconut", calories: 159}, 
+    //                 {id:5, name: "pineapple", calories: 37}];
 
 
     //fruits.sort((a,b) => a.name.localeCompare(b.namee));
